90f3acc20097ffb70ae074de811c3ecd8bb386e2
lukeesquivel <lukeesquivel@freshcut.gg>
2022-02-25 22:51:22 -0800

    fixed mana issue

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -2,14 +2,15 @@ import React from "react";
 import CardInHand from "./CardInHand";
 
 const Hand = (props) => {
-    return <div class="Hand">
-        {props.handProps.map( card =>
+    return <div className="Hand">
+        {props.handProps.map( (card, index) =>
             <CardInHand 
                 cardProps={card} 
                 dispatchPlayerActionsProps={props.dispatchPlayerActionsProps}
                 castSpellProps={props.castSpellProps}
                 playLandProps={props.playLandProps}
                 tapCardProps={props.tapCardProps}
+                key={index}
             /> 
         )}
     </div>
--- src/components/PlayingZone.js
+++ src/components/PlayingZone.js
@@ -3,11 +3,12 @@ import CardInPlay from "./CardInPlay"
 
 const PlayingZone = (props) => {
     console.log("PlayingZone component props is:", props)
-    return <div class="PlayingZone">
-        {props.cardsInPlayProps.map(card => 
+    return <div className="PlayingZone">
+        {props.cardsInPlayProps.map((card, index) => 
             <CardInPlay 
                 cardProps={card}
                 tapCardProps={props.tapCardProps}
+                key={index}
             />
         )}
     </div>
